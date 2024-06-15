505e3c8052c7a506647bcb84cd295cedf24ffc6c
ryjtoh <ryjtoh@gmail.com>
2024-06-14 19:17:25 -0700

    finished exercise

--- src/main/java/com/ryjtoh/beans/BeansApplication.java
+++ src/main/java/com/ryjtoh/beans/BeansApplication.java
@@ -1,13 +1,22 @@
 package com.ryjtoh.beans;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BeansApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeansApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Hello");
+		};
 	}
 
 }
--- src/main/java/com/ryjtoh/beans/controller/PostController.java
+++ src/main/java/com/ryjtoh/beans/controller/PostController.java
@@ -4,19 +4,27 @@ import com.ryjtoh.beans.service.PostService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
 
     private final PostService postService;
+    private final RestTemplate restTemplate;
 
-    public PostController(PostService postService) {
+    public PostController(PostService postService, RestTemplate restTemplate) {
         this.postService = postService;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping
     public String findAll() {
         return postService.findAll();
+    }
+
+    @GetMapping("/load")
+    public String loadPosts() {
+        return "load-posts";
     }
 }
